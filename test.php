6bab87307228c23fcbcd2a2c5c6f6e35ca27e86f
HaruMk2 <aida13@hotmail.co.jp>
2014-12-24 11:21:02 +0900

    10倍

--- test.php
+++ test.php
@@ -3,21 +3,21 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
-<title>PHPテスト</title>
+<title>PHPﾆ弾ﾆ湛ﾆ暖</title>
 </head>
 <body>
 
-<p>PHPのテストです。</p>
+<p>PHP窶堙姑弾ﾆ湛ﾆ暖窶堙�窶堋ｷﾂ。</p>
 
 <p>
 <?php
-$num1 = 320;
+$num1 = 3200;
 $num2 = $num1;
 
 print '$num1 ='.$num1.'<br />';
 print '$num2 ='.$num2.'<br />';
 
-$num1 = 18;
+$num1 = 180;
 
 print '$num1 ='.$num1.'<br />';
 print '$num2 ='.$num2;
